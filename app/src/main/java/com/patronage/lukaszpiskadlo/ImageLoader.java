5ade6e76b6bacce396fda282ac11090117216922
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2016-01-28 22:58:56 +0100

    added missing check

--- app/src/main/java/com/patronage/lukaszpiskadlo/ImageLoader.java
+++ app/src/main/java/com/patronage/lukaszpiskadlo/ImageLoader.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 
 public class ImageLoader {
     private Context context;
@@ -61,7 +62,7 @@ public class ImageLoader {
 
         if (imageLoaderTask != null) {
             String imageUrl = imageLoaderTask.imageUrl;
-            if (imageUrl.isEmpty()) {
+            if (imageUrl.isEmpty() || !imageUrl.equals(url)) {
                 imageLoaderTask.cancel(true);
             } else {
                 return false;
